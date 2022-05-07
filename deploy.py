406f003fcbd30d160c08eb61e0ae7514d3bdc12b
harshshaw <harshshaw5@gmail.com>
2022-05-07 22:59:14 +0530

    Updated the transaction calls and receipt

--- deploy.py
+++ deploy.py
@@ -21,7 +21,7 @@ compiled_sol = compile_standard(
             }
         },
     },
-     solc_version="0.6.0",
+    solc_version="0.6.0",
 )
 
 with open("compiled_code.json", "w")as file:
@@ -41,27 +41,48 @@ w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
 chain_id = 1337
 my_address = "0x3C5c9555697a21651D822e872Aac3854888f7Fd7"
 private_key = os.getenv("PRIVATE_KEY")
-print(private_key)
+
 
 # create the contract in python
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
 
-#Get the latest transaction
-nonce =w3.eth.getTransactionCount(my_address)
-print(nonce)
-#1. Build a transaction
-#2. Sign a transaction
-#3. Send a transaction
+# Get the latest transaction
+nonce = w3.eth.getTransactionCount(my_address)
 
-transaction=SimpleStorage.constructor().buildTransaction(
-    {"gasPrice": w3.eth.gas_price,"chainId":chain_id,"from":my_address,"nonce":nonce}
+# 1. Build a transaction
+# 2. Sign a transaction
+# 3. Send a transaction
+
+transaction = SimpleStorage.constructor().buildTransaction(
+    {"gasPrice": w3.eth.gas_price, "chainId": chain_id,
+        "from": my_address, "nonce": nonce}
 )
-signed_txn=w3.eth.account.sign_transaction(transaction,private_key =private_key)
-print(signed_txn)
+signed_txn = w3.eth.account.sign_transaction(
+    transaction, private_key=private_key)
 
-#send this signed transaction
-tx_hash=w3.eth.send_raw_transaction(signed_txn.rawTransaction)
-
+print("Deploying contracts...")
+# send this signed transaction
+tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed!")
 # Working with contract, you need:
-#Contract ABI
-#contract Address
+# Contract ABI
+# contract Address
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# call->Simulate  making the call and getting a return value
+# transact->Actually make a state change
+print(simple_storage.functions.retrieve().call())
+print("Updating contract...")
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {"gasPrice": w3.eth.gas_price, "chainId": chain_id,
+        "from": my_address, "nonce": nonce+1}
+)
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key)
+
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+print("Updated!")
+# transaction_hash = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+
+print(simple_storage.functions.retrieve().call())
