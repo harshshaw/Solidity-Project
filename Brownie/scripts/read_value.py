38341c14e1ec589d436243198a4d79841c720793
harshshaw <harshshaw5@gmail.com>
2022-05-12 15:19:38 +0530

    brownie console

--- Brownie/scripts/read_value.py
+++ Brownie/scripts/read_value.py
@@ -1,5 +1,10 @@
-from brownie import SimpleStorae, accounts, config
+from brownie import SimpleStorage, accounts, config
 
 
 def read_value():
-    pass
+    simple_storage = SimpleStorage[-1]
+    print(simple_storage.retrieve())
+
+
+def main():
+    read_value()
