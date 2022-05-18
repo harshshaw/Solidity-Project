f0ade54d4621d64671b795305789697a2159bf3c
harshshaw <harshshaw5@gmail.com>
2022-05-19 00:54:53 +0530

    Issue with accounts[0]

--- brownie_fund_me/scripts/deploy.py
+++ brownie_fund_me/scripts/deploy.py
@@ -1,15 +1,14 @@
-from cgi import print_exception
-from brownie import FundMe, network, config, MockV3Aggregator
-from scripts.helpful_scripts import get_account, deploy_mocks
-from web3 import Web3
+from brownie import FundMe, MockV3Aggregator, network, config
+from scripts.helpful_scripts import (
+    get_account,
+    deploy_mocks,
+    LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+)
 
 
 def deploy_fund_me():
     account = get_account()
-    # pass the price Feed address to our fundme contract
-    # if we are on a persistent netowrk like rinkeby, use the associated address
-    # otherwise deploy mocks
-    if network.show_active() != "development":
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
         price_feed_address = config["networks"][network.show_active()][
             "eth_usd_price_feed"
         ]
@@ -22,8 +21,8 @@ def deploy_fund_me():
         {"from": account},
         publish_source=config["networks"][network.show_active()].get("verify"),
     )
-
     print(f"Contract deployed to {fund_me.address}")
+    return fund_me
 
 
 def main():
